5a810f4d47aed813eca492e73944fd8a8a31371b
jose alfonso guzman cruz <bfjose17@gmail.com>
2022-10-18 23:00:56 -0500

    Generar modelo de base de datos antes creada en sqlServer

---
+++ APP-WindowsForms_crud-basic_ADO-NET/filedb/SqlQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_WindowsForms_crud_basic_ADO_NET.filedb
+{
+    class SqlQuery
+    {
+        /*
+        create database crudbasic
+            go
+        use crudbasic
+            go
+        create table persona(
+		id int identity(1,1),
+		nombre varchar(60),
+		apellido varchar(60),
+		correo varchar(60)
+		primary key(id)
+	    );
+         
+         */
+    }
+}
